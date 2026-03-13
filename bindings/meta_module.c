60ecf5d1f158563ffc67725011bf6db3747c9dd3
rmibdev <rmib.dev@gmail.com>
2026-03-13 15:17:58 +0000

    Return  odb4py version and other meta data

---
+++ bindings/meta_module.c
@@ -0,0 +1,44 @@
+/*
+ * odb4py
+ * Copyright (C) 2026 Royal Meteorological Institute of Belgium (RMI)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <stdio.h>
+
+// Version 
+#define ODB4PY_VERSION "1.1.3"
+
+// return version  
+static PyObject* odb_meta_version(PyObject *Py_UNUSED(self) ,  PyObject *Py_UNUSED(ignored)) {
+    return Py_BuildValue("s", ODB4PY_VERSION);
+}
+
+
+// Get platform , build etc  info 
+static PyObject* odb_meta_info(PyObject *Py_UNUSED(self), PyObject *Py_UNUSED(ignored)) {
+    return Py_BuildValue(
+        "{s:s, s:s, s:s, s:s}",
+        "version"     , ODB4PY_VERSION,
+        "compiler"    , Py_GetCompiler() ,
+        "platform"    , Py_GetPlatform() ,
+        "python_build", Py_GetBuildInfo()
+    );
+}
+
+
+// 
+/*static PyObject* odbMeta_env(PyObject *self, PyObject *Py_UNUSED(ignored)) {
+    //  set env variable for ODB 
+    //  setenv("ODB_IO_METHOD", "default", 0);
+    // 
+    return Py_BuildValue("s", "ODB environment stub initialized.");
+}*/
+
+
