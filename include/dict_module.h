83ddeed7b835692b90da8c867983a267d8562bc0
idir Dehmous <idirdehmous@gmail.com>
2026-03-14 19:24:30 +0100

    define the function as static

--- include/dict_module.h
+++ include/dict_module.h
@@ -1,6 +1,6 @@
 #ifndef DICT_MODULE_H
 #define DICT_MODULE_H
 
-PyObject *odb_dict_method(PyObject *self, PyObject *args, PyObject *kwargs)  ;
+static PyObject *odb_dict_method(PyObject *self, PyObject *args, PyObject *kwargs)  ;
 #endif
 
